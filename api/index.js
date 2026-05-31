327c9278125f950a17242d84766bf1b102e164cb
abhayff369-ctrl <abhayff369@gmail.com>
2026-05-31 11:19:59 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 // api/index.js
 // Multi-key: abhay1, abhay2, abhay3, abhay4, abhay5
-const VALID_KEYS = ['team', 'abhay2', 'abhay3', 'abhay4', 'abhay5'];
+const VALID_KEYS = ['team6months', 'abhay2', 'abhay3', 'abhay4', 'abhay5'];
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
